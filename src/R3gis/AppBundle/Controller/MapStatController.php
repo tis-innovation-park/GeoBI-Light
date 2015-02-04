44e1aba682ea7ca8256351a00186956b58f50345
root <root@geobi.r3-gis>
2015-02-04 10:04:54 +0100

    MS6 changes

--- src/R3gis/AppBundle/Controller/MapStatController.php
+++ src/R3gis/AppBundle/Controller/MapStatController.php
@@ -195,7 +195,7 @@ class MapStatController extends Controller {
         return $textPart;
     }
 
-    private function addClass(\ms_layer_obj $layer, $layerType, $expressionText, array $opt) {
+    private function addClass($layer, $layerType, $expressionText, array $opt) {
         $opt = array_merge(array('color' => null, 'outline_color' => null, 'opacity' => null, 'symbol' => 'circle', 'hightlight' => null), $opt);
         $msLayerType = $this->getMSGeometryType($layerType);
 
@@ -225,11 +225,11 @@ class MapStatController extends Controller {
         $class->updateFromString(implode("\n", $textPart));
     }
 
-    private function addNoDataClass(\ms_layer_obj $layer, $layerType, array $opt) {
+    private function addNoDataClass($layer, $layerType, array $opt) {
         $this->addClass($layer, $layerType, null, $opt);
     }
 
-    private function addChartClass(\ms_layer_obj $layer, $dataColumn, $expressionText, array $opt) {
+    private function addChartClass($layer, $dataColumn, $expressionText, array $opt) {
         $opt = array_merge(array('color' => null, 'outline_color' => null, 'opacity' => null, 'offset' => 0), $opt);
 
         $class = ms_newClassObj($layer);
@@ -359,6 +359,7 @@ class MapStatController extends Controller {
                 $layer->setprocessing("CHART_SIZE={$w} {$h}");
                 $layer->setprocessing("CHART_TYPE=bar");
             }
+            
             $layer->set('data', "the_geom FROM ({$sql}) AS foo USING UNIQUE gid USING SRID={$mapInfo['srid']}");
 
             $tot = count($mapInfo['class']);
@@ -395,6 +396,7 @@ class MapStatController extends Controller {
                     'outline_color' => $mapInfo['outline_color'],
                     'opacity' => $opacity));
             } else if ($mapInfo['ms_layer_type'] == 'point' && $mapInfo['size_type'] == 'variable') {
+                
                     $totClasses = count($mapInfo['class']);
                     $size = $mapInfo['min_size'];
                     $delta = abs($mapInfo['max_size'] - $mapInfo['min_size']) / ($totClasses - 1);
@@ -469,6 +471,7 @@ class MapStatController extends Controller {
             /* Eexecute request */
             $this->map->owsdispatch($objRequest);
             // $this->map->save('/tmp/geobi.map');
+
 
             $contenttype = ms_iostripstdoutbuffercontenttype();
             // ONLY IN DEV MODE
